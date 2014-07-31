bf5d2d7c34baf1e746ecc714d55269e7bfe07b8e
Brandon Myers <bdmyers@cs.washington.edu>
2014-07-31 14:30:59 -0700

    use right python

--- start-collect-hca.sh
+++ start-collect-hca.sh
@@ -2,8 +2,10 @@ samples=${1:-1800}
 
 PERF_TOOLS=/people/bdmyers/escience/perf-tools
 
+python=`which python`
+
 nodes=`scontrol show hostname $SLURM_NODELIST`
 localdir=`pwd`
 for x in $nodes; do
-  ssh $x python $PERF_TOOLS/collect_hca.py $samples > $localdir/$x.hca.$SLURM_JOB_ID.log && echo "$x done" &
+  ssh $x $python $PERF_TOOLS/collect_hca.py $samples $localdir/$x.hca.$SLURM_JOB_ID.log && echo "$x done" &
 done
