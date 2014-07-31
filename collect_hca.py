bf16dc7e36ad4c45f08b704be9bc938d8bbbee48
Brandon Myers <bdmyers@cs.washington.edu>
2014-07-31 14:30:52 -0700

    flush every line

--- collect_hca.py
+++ collect_hca.py
@@ -8,6 +8,9 @@ import sys
 samples = -1
 if len(sys.argv) > 1:
     samples = int(sys.argv[1])
+logfilen = socket.gethostname()+'.log'
+if len(sys.argv) > 2:
+    logfilen = sys.argv[2]
 sc = 0
 
 ctr_path = '/sys/class/infiniband/mlx4_0/ports/1/counters/'
@@ -19,29 +22,31 @@ mark_resets = False
 def get_counters():
     return [int(subprocess.check_output(['cat', ctr_path+ctr_name])) for ctr_name in counters]
 
-print " ".join(['time']+counters)
+with open(logfilen, 'w') as fout:
+    fout.write(" ".join(['time']+counters) + '\n')
 
-last = get_counters()
-last_time = time.time()
+    last = get_counters()
+    last_time = time.time()
 
-while(samples==-1 or sc < samples):
-    sc+=1
-    current = get_counters()
-    current_time = time.time()
+    while(samples==-1 or sc < samples):
+        sc+=1
+        current = get_counters()
+        current_time = time.time()
 
-    # take the delta and account for external collectl resets
-    delta = [float(c - l if c-l>=0 else c)/(current_time-last_time) for (c,l) in zip(current, last)]
-    
-    if mark_resets:
-        reset_occurred = any([c-l < 0 for (c,l) in zip(current,last)])
-        if reset_occurred: print "RESET"
+        # take the delta and account for external collectl resets
+        delta = [float(c - l if c-l>=0 else c)/(current_time-last_time) for (c,l) in zip(current, last)]
+        
+        if mark_resets:
+            reset_occurred = any([c-l < 0 for (c,l) in zip(current,last)])
+            if reset_occurred: print "RESET"
 
-    outputs = [current_time] + delta
+        outputs = [current_time] + delta
 
-    print " ".join([str(x) for x in outputs])
-    time.sleep(1)
+        fout.write(" ".join([str(x) for x in outputs]) + '\n')
+        fout.flush() # I want it now!
+        time.sleep(1)
 
-    last = current
-    last_time = current_time
+        last = current
+        last_time = current_time
 
 
