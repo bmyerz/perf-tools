95ca08b0fc45d56b09e5960c930685322129e1f9
Brandon Myers <bdmyers@cs.washington.edu>
2014-07-31 15:12:07 -0700

    burns

--- collect_logs.py
+++ collect_logs.py
@@ -2,8 +2,14 @@ import glob
 import subprocess
 import re
 import csv
+import sys
 
-fnpat = re.compile(r'([a-zA-Z]+[0-9]+).([a-zA-Z]+[0-9]+)')
+burns = 0
+if len(sys.argv) > 1:
+    burns = int(sys.argv[1])
+
+if filepat==0:
+    fnpat = re.compile(r'([a-zA-Z]+[0-9]+).([a-zA-Z]+[0-9]+)')
 
 with open ('logs.csv', 'w') as csvout:
     cw = csv.writer(csvout, delimiter=',')
@@ -16,7 +22,11 @@ with open ('logs.csv', 'w') as csvout:
 
     for logfile in glob.glob("*.log"):
         with open(logfile, 'r') as f:
+            lc = 0
             for l in f:
+                lc+=1
+                if lc <= burns: continue
+
                 cols = l.split()
                 hostname = fnpat.match(logfile).group(1)
                 cols.append(hostname)
