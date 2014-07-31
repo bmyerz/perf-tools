da46ad8c28d2e45f143a64407f976023f2b7215d
Brandon Myers <bdmyers@cs.washington.edu>
2014-07-30 18:24:10 -0700

    docs

--- start-collect-iface.sh
+++ start-collect-iface.sh
@@ -1,8 +1,8 @@
-samples=$1
-iface_pattern=$2
+samples=$1  # number of samples to take; note you can instead stop all collection with stop-collect-all.sh
+iface_pattern=$2  # e.g., ib0, eth0, ...
 
 nodes=`scontrol show hostname $SLURM_NODE_LIST`
 localdir=`pwd`
 for x in $nodes; do
-  ssh $x sar -n DEV 1 $samples | grep $iface_pattern > $localdir/$x.$SLURM_JOB_ID.log && echo "$x done" &
+  ssh $x sar -n DEV 1 $samples | grep $iface_pattern > $localdir/$x.$iface_pattern.$SLURM_JOB_ID.log && echo "$x done" &
 done
