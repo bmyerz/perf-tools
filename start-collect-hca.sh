841655d37089a74430fe7b42fffac2534f7b6a18
Brandon Myers <bdmyers@cs.washington.edu>
2014-07-31 15:12:23 -0700

    change name of log files

--- start-collect-hca.sh
+++ start-collect-hca.sh
@@ -5,5 +5,5 @@ PERF_TOOLS=/people/bdmyers/escience/perf-tools
 nodes=`scontrol show hostname $SLURM_NODELIST`
 localdir=`pwd`
 for x in $nodes; do
-  ssh $x python $PERF_TOOLS/collect_hca.py $samples $localdir/$x.hca.$SLURM_JOB_ID.log && echo "$x done" &
+  ssh $x python $PERF_TOOLS/collect_hca.py $samples $localdir/$x.hca1.$SLURM_JOB_ID.log && echo "$x done" &
 done
