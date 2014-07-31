74715e87887f49f1b5a9d43141a56d2dbd205941
Brandon Myers <bdmyers@cs.washington.edu>
2014-07-31 14:31:58 -0700

    make stop scripts faster and block

--- stop-collect-all.sh
+++ stop-collect-all.sh
@@ -2,5 +2,6 @@
 nodes=`scontrol show hostname $SLURM_NODELIST`
 for x in $nodes; do
   pid=`ssh $x ps aux | grep sar | grep -v grep | awk '{ print $2 }'`
-  ssh $x kill $pid
+  ssh $x kill $pid &
 done
+wait
--- stop-collect-hca.sh
+++ stop-collect-hca.sh
@@ -2,5 +2,6 @@
 nodes=`scontrol show hostname $SLURM_NODELIST`
 for x in $nodes; do
   pid=`ssh $x ps aux | grep collect_hca | grep -v grep | awk '{ print $2 }'`
-  ssh $x kill $pid
+  ssh $x kill $pid && echo "killed $x" &
 done
+wait
